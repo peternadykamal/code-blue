d23300bca02df4a119588e261abc41d8ab1fda92
HaneenAyman223 <haneen52000@hotmail.com>
2023-03-21 20:07:49 +0200

    buildgradle

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gradproject/splashscreen.dart';
 
-
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 }
 
 class MyApp extends StatelessWidget {
@@ -14,15 +15,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home: splashScreen() ,
+      home: splashScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
-
-
-
-

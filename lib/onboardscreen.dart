9050128924bc894a368e02cf4722f2a262b7346a
M3rfan11 <Me5280908@gmail.com>
2023-04-20 04:57:44 +0200

    onboardingdonecommit

--- lib/onboardscreen.dart
+++ lib/onboardscreen.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:gradproject/sos.dart';
+import 'package:gradproject/splashscreen.dart';
 import 'package:gradproject/style.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class onBoardScreen extends StatefulWidget {
-  const onBoardScreen({super.key});
+  const onBoardScreen({key});
 
   @override
   State<onBoardScreen> createState() => _onBoardScreenState();
@@ -14,6 +17,8 @@ class onBoardScreen extends StatefulWidget {
 
 class _onBoardScreenState extends State<onBoardScreen> {
   final controller = PageController();
+  bool isLastPage = false;
+
   @override
   void dispose() {
     controller.dispose();
@@ -32,6 +37,9 @@ class _onBoardScreenState extends State<onBoardScreen> {
                   fit: BoxFit.fill)),
           child: PageView(
             controller: controller,
+            onPageChanged: (index) {
+              setState(() => isLastPage = index == 4);
+            },
             children: [
               Container(
                   child: Column(
@@ -48,12 +56,16 @@ class _onBoardScreenState extends State<onBoardScreen> {
                   Text("SOS BUTTON",
                       style:
                           TextStyle(fontSize: 15, color: Mycolors.textcolor)),
-                  Text(
-                      "With just one press, our SOS button will \n automatically order an ambulance to your current\n location, send your medical card to the \n paramedic, and notify your caregivers via SMS \n and app notification of your critical situation and \n location.",
-                      style: TextStyle(
+                  Center(
+                    child: Text(
+                        "With just one press, our SOS button will \n automatically order an ambulance to your current\n location, send your medical card to the \n paramedic, and notify your caregivers via SMS \n and app notification of your critical situation and \n location.",
+                        style: TextStyle(
                           color: Mycolors.textcolor,
                           fontSize: 13,
-                          fontWeight: FontWeight.bold))
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center),
+                  )
                 ],
               )),
               Container(
@@ -73,12 +85,15 @@ class _onBoardScreenState extends State<onBoardScreen> {
                   Text("MEDICAL CARD",
                       style:
                           TextStyle(fontSize: 15, color: Mycolors.textcolor)),
-                  Text(
-                      "By filling out your medical information in our\n application, paramedics will be able to quickly\n access vital information about your health,\n allergies, medications, and more, potentially \n saving your life.",
-                      style: TextStyle(
-                          color: Mycolors.textcolor,
-                          fontSize: 13,
-                          fontWeight: FontWeight.bold))
+                  Center(
+                    child: Text(
+                        "By filling out your medical information in our\n application, paramedics will be able to quickly\n access vital information about your health,\n allergies, medications, and more, potentially \n saving your life.",
+                        style: TextStyle(
+                            color: Mycolors.textcolor,
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center),
+                  )
                 ],
               )),
               Container(
@@ -96,12 +111,15 @@ class _onBoardScreenState extends State<onBoardScreen> {
                   Text("EMERGENCY SMS BACKUP",
                       style:
                           TextStyle(fontSize: 15, color: Mycolors.textcolor)),
-                  Text(
-                      "By enabling this feature, our app will send an SMS \n to the server and all the designated caregivers \n notifying them of the emergency if there is no \n internet connection available.",
-                      style: TextStyle(
-                          color: Mycolors.textcolor,
-                          fontSize: 13,
-                          fontWeight: FontWeight.bold))
+                  Center(
+                    child: Text(
+                        "By enabling this feature, our app will send an SMS \n to the server and all the designated caregivers \n notifying them of the emergency if there is no \n internet connection available.",
+                        style: TextStyle(
+                            color: Mycolors.textcolor,
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center),
+                  )
                 ],
               )),
               Container(
@@ -119,12 +137,15 @@ class _onBoardScreenState extends State<onBoardScreen> {
                   Text("CHATBOT",
                       style:
                           TextStyle(fontSize: 15, color: Mycolors.textcolor)),
-                  Text(
-                      " Our chatbot is available in both English and \n Arabic languages, providing first aid information \n and assistance to help you handle a medical \n emergency.",
-                      style: TextStyle(
-                          color: Mycolors.textcolor,
-                          fontSize: 13,
-                          fontWeight: FontWeight.bold))
+                  Center(
+                    child: Text(
+                        " Our chatbot is available in both English and \n Arabic languages, providing first aid information \n and assistance to help you handle a medical \n emergency.",
+                        style: TextStyle(
+                            color: Mycolors.textcolor,
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center),
+                  )
                 ],
               )),
               Container(
@@ -142,12 +163,15 @@ class _onBoardScreenState extends State<onBoardScreen> {
                   Text("NEARBY HOSPITALS & AMBULANCE ORDERING",
                       style:
                           TextStyle(fontSize: 15, color: Mycolors.textcolor)),
-                  Text(
-                      "You can view nearby hospitals and order an \n ambulance from the location you choose, giving \n you quick and convenient access to medical help.",
-                      style: TextStyle(
-                          color: Mycolors.textcolor,
-                          fontSize: 13,
-                          fontWeight: FontWeight.bold))
+                  Center(
+                    child: Text(
+                        "You can view nearby hospitals and order an \n ambulance from the location you choose, giving \n you quick and convenient access to medical help.",
+                        style: TextStyle(
+                            color: Mycolors.textcolor,
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center),
+                  )
                 ],
               )),
             ],
@@ -156,13 +180,8 @@ class _onBoardScreenState extends State<onBoardScreen> {
         bottomSheet: Container(
           color: Mycolors.splashback,
           height: 80,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
             children: [
-              TextButton(
-                  onPressed: () => controller.jumpToPage(4),
-                  child: Text("SKIP",
-                      style: TextStyle(color: Mycolors.notpressed))),
               Center(
                 child: SmoothPageIndicator(
                   onDotClicked: (index) => controller.animateToPage(index,
@@ -171,15 +190,58 @@ class _onBoardScreenState extends State<onBoardScreen> {
                   controller: controller,
                   count: 5,
                   effect: WormEffect(
+                      dotHeight: 10,
+                      dotWidth: 10,
                       activeDotColor: Mycolors.buttonsos,
                       dotColor: Mycolors.fillingcolor),
                 ),
               ),
-              TextButton(
-                  onPressed: () => controller.nextPage(
-                      duration: Duration(milliseconds: 500),
-                      curve: Curves.easeInOut),
-                  child: Text("NEXT")),
+              Container(
+                margin: EdgeInsets.only(left: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                        onPressed: () => controller.jumpToPage(4),
+                        child: Text("SKIP",
+                            style: TextStyle(
+                                color: Mycolors.notpressed, fontSize: 16))),
+                    InkWell(
+                        onTap: () async {
+                          // final Prefs = await SharedPreferences.getInstance();
+                          // Prefs.setBool('showHome', true);
+                          isLastPage
+                              ? Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => splashScreen()))
+                              : controller.nextPage(
+                                  duration: Duration(milliseconds: 500),
+                                  curve: Curves.easeInOut);
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(right: 20),
+                          height: 34,
+                          width: 96,
+                          decoration: BoxDecoration(
+                              color: Mycolors.buttonsos,
+                              borderRadius: BorderRadius.circular(5)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                "Next",
+                                style: TextStyle(
+                                    color: Mycolors.fillingcolor, fontSize: 16),
+                              ),
+                              Icon(Icons.chevron_right_sharp,
+                                  color: Mycolors.fillingcolor)
+                            ],
+                          ),
+                        )),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
 
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:gradproject/main.dart';
+import 'package:gradproject/sos.dart';
 import 'package:gradproject/style.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
@@ -18,31 +20,38 @@ class splashScreen extends StatefulWidget {
 }
 
 class _splashScreenState extends State<splashScreen> {
-    @override
+  @override
   @override
   void initState() {
-  
     super.initState();
-    Timer(Duration(seconds: 10), () async => {
-      await context.setLocale(Locale(langCode)),
-      Navigator.pushReplacement(context, MaterialPageRoute(builder:(context) => authPage() ))
-    } );
+    Timer(
+        Duration(seconds: 10),
+        () async => {
+              await context.setLocale(Locale(langCode)),
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          FirebaseAuth.instance.currentUser == null
+                              ? authPage()
+                              : sosPage()))
+            });
   }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-    
-    backgroundColor: Mycolors.splashback,  
-    body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Center(child: Image.asset("assets/images/codeblueslogo1.png")),
-        SizedBox(height: 180),
-        LoadingAnimationWidget.threeArchedCircle(color: Colors.white, size: 40),
-      ],
-    ),    
-        
+        backgroundColor: Mycolors.splashback,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(child: Image.asset("assets/images/codeblueslogo1.png")),
+            SizedBox(height: 180),
+            LoadingAnimationWidget.threeArchedCircle(
+                color: Colors.white, size: 40),
+          ],
+        ),
       ),
     );
   }

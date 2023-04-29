490341df90cdb7a866edc09db34ebaef8c6a13e9
peter nady <nady.peter347@gmail.com>
2023-04-29 10:22:35 +0200

    1. including the firebase function with the dataset inside the project 2. adding slight changes to pop up menu in the profile page

--- firebase sdk python/database.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, db
-
-cred = credentials.Certificate("./serviceAccountKey.json")
-firebase_admin.initialize_app(
-    cred, {"databaseURL": "https://codeblue-482a2-default-rtdb.firebaseio.com/"})
-
-ref = db.reference()
-print(ref.get())
